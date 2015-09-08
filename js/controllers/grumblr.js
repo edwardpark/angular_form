3d7f9ba7818dd25f09f5745549070d93b0e72648
Edward Park <parkes.edward@gmail.com>
2015-09-08 11:03:01 -0400

    added init and established first MV-VM

---
+++ js/controllers/grumblr.js
@@ -0,0 +1,50 @@
+var app = angular.module('grumblr');
+
+app.controller('grumblrController',function(){
+
+
+
+
+
+
+
+
+  this.list = [
+    {
+      title:"Fuck that Sun, Lets drink and Fight",
+      author_name:"Earnest HemingWay ",
+      content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+    },
+    {
+      title:"I am Dali",
+      author_name:"Salivdor Dali ",
+      content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+    },
+    {
+      title:"I am a genius but lonely",
+      author_name:"Dorothy Leggings",
+      content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+    },
+    {
+      title:"I am a PornStar and Body Cavity Smuggler Expert",
+      author_name:"Dauny Deep",
+      content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+    },
+    {
+      title:"I am Matt Damon",
+      author_name:"Matt Damon",
+      content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+    },
+    {
+      title:"I hate everyone",
+      author_name:"Red Foreman",
+      content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+    }
+  ]
+});
