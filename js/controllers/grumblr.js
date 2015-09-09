b880d848b077a19ddaf45f30f2695f3aefcbaffd
Edward Park <parkes.edward@gmail.com>
2015-09-09 07:19:56 -0400

    added comment display

--- js/controllers/grumblr.js
+++ js/controllers/grumblr.js
@@ -42,37 +42,145 @@ app.controller('grumblrController',function(){
       title:"Lets drink and Fight",
       author_name:"Earnest HemingWay ",
       content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
-      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg ",
+      comments:[
+        {
+          comment: " this is comment 1",
+          comment_author:" Randy Johnson "
+        },
+        {
+          comment: " this is comment 2",
+          comment_author:" Bandy Johnson "
+        },
+        {
+          comment: " this is comment 3",
+          comment_author:" Dandy Johnson "
+        },
+        {
+          comment: " this is comment 4",
+          comment_author:" Gandy Johnson "
+        }
+      ]
     },
     {
       title:"I am Dali",
       author_name:"Salivdor Dali ",
       content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
-      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg ",
+      comments:[
+        {
+          comment: " this is comment 1",
+          comment_author:" Randy Johnson "
+        },
+        {
+          comment: " this is comment 2",
+          comment_author:" Bandy Johnson "
+        },
+        {
+          comment: " this is comment 3",
+          comment_author:" Dandy Johnson "
+        },
+        {
+          comment: " this is comment 4",
+          comment_author:" Gandy Johnson "
+        }
+      ]
     },
     {
       title:"I am a genius but lonely",
       author_name:"Dorothy Leggings",
       content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
-      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg ",
+      comments:[
+        {
+          comment: " this is comment 1",
+          comment_author:" Randy Johnson "
+        },
+        {
+          comment: " this is comment 2",
+          comment_author:" Bandy Johnson "
+        },
+        {
+          comment: " this is comment 3",
+          comment_author:" Dandy Johnson "
+        },
+        {
+          comment: " this is comment 4",
+          comment_author:" Gandy Johnson "
+        }
+      ]
     },
     {
       title:"I am a PornStar and Body Cavity Smuggler Expert",
       author_name:"Dauny Deep",
       content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
-      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg ",
+      comments:[
+        {
+          comment: " this is comment 1",
+          comment_author:" Randy Johnson "
+        },
+        {
+          comment: " this is comment 2",
+          comment_author:" Bandy Johnson "
+        },
+        {
+          comment: " this is comment 3",
+          comment_author:" Dandy Johnson "
+        },
+        {
+          comment: " this is comment 4",
+          comment_author:" Gandy Johnson "
+        }
+      ]
     },
     {
       title:"I am Matt Damon",
       author_name:"Matt Damon",
       content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
-      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg ",
+      comments:[
+        {
+          comment: " this is comment 1",
+          comment_author:" Randy Johnson "
+        },
+        {
+          comment: " this is comment 2",
+          comment_author:" Bandy Johnson "
+        },
+        {
+          comment: " this is comment 3",
+          comment_author:" Dandy Johnson "
+        },
+        {
+          comment: " this is comment 4",
+          comment_author:" Gandy Johnson "
+        }
+      ]
     },
     {
       title:"I hate everyone",
       author_name:"Red Foreman",
       content:"Cras et felis nisi. Phasellus bibendum mauris quis ante aliquam, eget consequat urna facilisis. Curabitur semper ligula vitae neque dapibus congue. Aliquam posuere massa quis lacus vulputate, id sollicitudin urna fringilla. Vivamus eu cursus risus, ut bibendum lacus.  ",
-      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg "
+      photo_url:"http://www.dogbreedinfo.com/images10/PembrokeLucy2.jpg ",
+      comments:[
+        {
+          comment: " this is comment 1",
+          comment_author:" Randy Johnson "
+        },
+        {
+          comment: " this is comment 2",
+          comment_author:" Bandy Johnson "
+        },
+        {
+          comment: " this is comment 3",
+          comment_author:" Dandy Johnson "
+        },
+        {
+          comment: " this is comment 4",
+          comment_author:" Gandy Johnson "
+        }
+      ]
     }
   ]
 });
